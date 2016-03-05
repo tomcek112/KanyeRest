05a85a608cec0924154501bcb2d3777c81015542
Deirdre Bringas <deirdrebringas@gmail.com>
2016-03-05 15:02:48 +0000

    meh

--- tweet.py
+++ tweet.py
@@ -3,14 +3,16 @@ from tweepy import OAuthHandler
 from pymarkovchain import MarkovChain
 import requests, time
 import re
+from daemon import runner 
+
 r = requests.get('http://www.kanyerest.xyz/api/lyrics')
 #print r.content
 
 
-consumer_key = 'ZdOfyIbTOauE15k2LChIE09du'
-consumer_secret = 'XhAa1HUqU6oYieH746KAcICw8oRbOqtpxMOSXMUun724pRnJhU'
-access_token = '706117375863689217-jPUo2aw3r8mSYKzpWIirm2b9KpPA8nN'
-access_secret = 'H0nCKygT1RybhSmrwKCtkP6koZFKJzPfUabTssGURIPoL'
+consumer_key = 'CONSUMER-KEY'
+consumer_secret = 'CONSUMER-SECRET'
+access_token = 'ACCESS-TOKEN'
+access_secret = 'ACCESS-SECRET'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
