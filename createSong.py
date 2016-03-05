c336e86aea3708c40b9ccb255ebd0e9a56d9e0a1
Deirdre Bringas <deirdrebringas@gmail.com>
2016-03-05 21:57:05 +0000

    create song

---
+++ createSong.py
@@ -0,0 +1,36 @@
+from pymarkovchain import MarkovChain
+import requests, time
+import re
+
+r = requests.get('http://www.kanyerest.xyz/api/lyrics')
+mc = MarkovChain("")
+mc.generateDatabase(r.content)
+
+def writeLine():
+	tweet = mc.generateString()
+	tweet = tweet[:140].rsplit(r'\n', 1)[0]
+	return tweet
+
+def createRhymePair():
+	song = []
+	line1 = re.sub(r'\)|\(', '', writeLine())
+	song.append(line1)
+	line2 = re.sub(r'\)|\(', '', writeLine())
+	while line2[-2:] != line1[-2:]:
+		line2 = re.sub(r'\)|\(', '', writeLine())
+	song.append(line2)
+	return song
+
+
+def createSong(n):
+	song = []
+	for i in range(0,n-1):
+		song.append(createRhymePair())
+	return song
+
+def printSong(n):
+	lyrics = []
+	for line in createSong(n):
+		lyrics.append(line[0])
+		lyrics.append(line[1])
+	return lyrics
